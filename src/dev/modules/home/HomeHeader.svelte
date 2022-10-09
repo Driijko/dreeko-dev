9dcc44047936a098449ef7f4ffc307760bae781d
Driijko <driijko@gmail.com>
2022-10-09 00:07:03 -0400

    Add text and scroll indicator to HomeHeader module.

--- src/dev/modules/home/HomeHeader.svelte
+++ src/dev/modules/home/HomeHeader.svelte
@@ -7,28 +7,30 @@
   // LIFECYCLE --------------------------------------------------------
   onMount(()=> {
     const tl = gsap.timeline();
+    tl.set(".line-group1", {opacity: 1});
+    tl.set(".line-group2", {opacity: 1});
     tl.from(".line-group1", {
       delay: 2,
-      duration: 3.5,
+      duration: 7,
       attr: {
         y1: "100%",
         x1: "100%",
       },
       stagger: 0.15,
       strokeWidth: 100,
-      ease: "power2.out",
+      ease: "power1.out",
       stroke: "hsla(220, 100%, 100%, 1)",
-    }, 0);
+    }, 0.1);
     tl.from(".line-group2", {
       delay: 2,
-      duration: 3.5,
+      duration: 7,
       attr: {
         y2: "0%",
         x2: "0%",
       },
       stagger: 0.15,
       strokeWidth: 100,
-      ease: "power2.out",
+      ease: "power1.out",
       stroke: "hsla(220, 100%, 100%, 1)",
     }, 0);
   });
@@ -36,7 +38,11 @@
 
 <!-- MARKUP ///////////////////////////////////////////////////////// -->
 <div>
-  <svg>
+  <h2>Aesthetically-driven<br/> Web Development</h2>
+  <svg id="scroll-indicator" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
+  </svg>
+  <svg id="animation">
     <line class="line-group1" x1="-5%" y1="-5%" x2="100%" y2="100%" />
     <line class="line-group1" x1="0%" y1="-5%" x2="100%" y2="100%" />
     <line class="line-group1" x1="5%" y1="-5%" x2="100%" y2="100%" />
@@ -88,19 +94,57 @@
 <style>
   div {
     height: calc(var(--vph) - 7.5rem);
-    /* background-color: var(--color1); */
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    row-gap: 2rem;
   }
-  svg {
+  @keyframes background-fade-in {
+    from {background-color: hsla(220, 100%, 10%, 0);}
+    to {background-color: hsla(220, 100%, 8%, 0.9);}
+  }
+  h2{
+    font-size: 2rem;
+    line-height: 1.5;
+    letter-spacing: 0.1rem;
+    word-spacing: 0.1rem;
+    text-align: center;
+    color: var(--color2);
+    font-family: "Julius Sans One",sans-serif;
+    animation: background-fade-in 5s 8s ease-out forwards;
+    width: 100%;
+    padding: 1rem 0;
+  }
+  #animation {
+    position: absolute;
+    top: 0;
+    left:0;
     width: 100%;
     height: 100%;
-    /* opacity: 0; */
-    /* animation: fade-in 5s 2s forwards ease-out; */
+    z-index: -1;
   }
 
   line {
     stroke: var(--color1);
     stroke-width: 1vw;
     stroke-linecap: round;
+    opacity: 0;
+  }
+
+  #scroll-indicator {
+    padding: 0.7rem;
+    width: 3.5rem;
+    color: var(--color2);
+    border-radius: 50%;
+    animation: background-fade-in 5s 8s ease-out forwards;
+  }
+
+  @media screen and (min-width:600px) {
+    h2 {
+      font-size: 3rem;
+    }
   }
 
   @media screen and (min-width: 1280px) {
