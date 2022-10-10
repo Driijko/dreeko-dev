0b7e4f1fddf0d2c0d26aad3078d0993f4d4005ea
Driijko <driijko@gmail.com>
2022-10-10 18:10:22 -0400

    Add resize listener to setup function.

--- src/dev/stores/site.js
+++ src/dev/stores/site.js
@@ -3,14 +3,25 @@ import { writable } from "svelte/store";
 
 // SETTINGS --------------------------------------------------------
 const pageExitDuration = 3000;
+const resizeDelay = 2000;
 
 // SETUP ----------------------------------------------------------
 export function setUp() {
-  // Page exit duration
+  // CSS Variables
   document.documentElement.style.setProperty("--page-exit-duration", `${pageExitDuration}ms`);
   document.documentElement.style.setProperty("--viewport-height", `${window.innerHeight}px`);
+
+  // RESIZE
+  window.addEventListener("resize", ()=> {
+    const timerId = setTimeout(()=> {
+      document.documentElement.style.setProperty("--viewport-height", `${window.innerHeight}px`);
+      clearTimeout(timerId);
+    }, resizeDelay);
+  });
 }
 
+// CURRENT PAGE -----------------------------------------------------
+// with page exit design included
 function createCurrentPage() {
   const { subscribe, set } = writable("loading-screen");
 
@@ -27,3 +38,5 @@ function createCurrentPage() {
 }
 
 export const currentPage = createCurrentPage();
+
+// RESIZE
